86d3a476ab602c6caeb746b8b406f2549605292d
Marie <marie@test.openesdh.dk>
2016-04-20 14:34:35 +0200

    close and (fake)submit new site form

--- app/src/sites/SitesController.js
+++ app/src/sites/SitesController.js
@@ -7,35 +7,21 @@
         function SitesController($scope, $mdDialog) {
 					
 					$scope.getSites = function(event) {
-
-					// var confirm = $mdDialog.prompt()
-	// 				          .title('Create new site')
-	// 				          .textContent('Here we need site name and description.')
-	// 				          .placeholder('Name')
-	// 				          .ariaLabel('Site name')
-	// 				          .targetEvent(event)
-	// 				          .ok('Create')
-	// 				          .cancel('Cancel');
-	// 				    $mdDialog.show(confirm).then(function(result) {
-	// 				      $scope.status = 'New site created!';
-	// 				    }, function() {
-	// 				      $scope.status = 'Did not create new site.';
-	// 				    });	
-
-	    $mdDialog.show({
-	      templateUrl: 'app/src/sites/view/newProject.tmpl.html',
-	      targetEvent: event,
-	      clickOutsideToClose:true	    
-			})
-	    .then(function(answer) {
-	      $scope.status = 'You said the information was .';
-	    }, function() {
-	      $scope.status = 'You cancelled the dialog.';
-	    });
-								
+	   				$mdDialog.show({
+	   				  templateUrl: 'app/src/sites/view/newProject.tmpl.html',
+							parent: angular.element(document.body),
+	   				  targetEvent: event,
+	   				  clickOutsideToClose:true
+		 				});
+					}; // getSites close
 					
-					
-					
+					$scope.cancel = function() {
+					  $mdDialog.cancel();
 					};
-        };
+					
+					$scope.testSite = function(name, description) {
+						console.log(name + ", " + description);
+					}
+					
+        }; // SiteCtrl close
         
