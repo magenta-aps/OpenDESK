d8fc345d48d11bf514c2041babaf60e6b8f7a00e
flemmingheidepedersen <fhp@magenta-aps.dk>
2016-10-10 08:47:22 +0200

    added testsetup

--- runTests.sh
+++ runTests.sh
@@ -1,14 +1,15 @@
-cd  /var/lib/jenkins/jobs/opendesk-ui/workspace
+#cd  /var/lib/jenkins/jobs/opendesk-ui/workspace
 # cd $BASEDIR
 
 declare -i LOOP_COUNT=0
 declare -i MAX_LOOP_COUNT=120
 
-response=$(curl --write-out %{http_code} --silent --output /dev/null  http://178.62.194.129//alfresco/service/api/login\?u=admin\&pw=bullerfisk1992)
+#response=$(curl --write-out %{http_code} --silent --output /dev/null  http://178.62.194.129//alfresco/service/api/login\?u=admin\&pw=bullerfisk1992)
+response=$(curl --write-out %{http_code} --silent --output /dev/null  http://localhost:8080/alfresco/service/api/login\?u=admin\&pw=admin)
 while [ "$response" -ne "200" ]
 do
 	echo -e "\n-----> OPEN_E_TEST:Waiting for alfresco to start (not able to login as admin)"
-	response=$(curl --write-out %{http_code} --silent --output /dev/null  http://178.62.194.129//alfresco/service/api/login\?u=admin\&pw=bullerfisk1992)
+	response=$(curl --write-out %{http_code} --silent --output /dev/null  http://localhost:8080/alfresco/service/api/login\?u=admin\&pw=admin)
 	sleep 5
 	((LOOP_COUNT++))
         if [ "$LOOP_COUNT" -gt "$MAX_LOOP_COUNT" ]; then
