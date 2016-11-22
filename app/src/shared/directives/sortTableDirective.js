297849c1612c27d4eb1edc9ff7d5cafc6b955eb7
Heine <heine@magenta.dk>
2016-11-22 14:33:33 +0100

    Sorting on columns

--- app/src/shared/directives/sortTableDirective.js
+++ app/src/shared/directives/sortTableDirective.js
@@ -4,16 +4,22 @@
         .directive('sortTable', sortTable);
 
     function sortTable($compile){
+		
         function sortLink(scope, element, attrs){
             element
                 .append('<i class="material-icons" ng-if="reverseOrder && sortType == \''+attrs.sortTable+'\'">keyboard_arrow_down</i>')
-                .append('<i class="material-icons" ng-if="!reverseOrder && sortType == \''+attrs.sortTable+'\'">keyboard_arrow_up</i>');
-
+                .append('<i class="material-icons" ng-if="!reverseOrder && sortType == \''+attrs.sortTable+'\'">keyboard_arrow_up</i>');				
+				
             $compile(element.contents())(scope);
 
             if ('sortDefault' in attrs) {
                 scope.orderByAttribute = attrs.sortTable;
                 scope.reverseOrder = attrs.sortDefault=='reverse' ? true : false;
+				if (scope.reverseOrder) {
+					element.append('<i class="material-icons">keyboard_arrow_down</i>');
+				} else {
+					element.append('<i class="material-icons">keyboard_arrow_up</i>');
+				}
             }
 
             element.bind('click', function(element){
@@ -21,9 +27,11 @@
                 scope.reverseOrder = !scope.reverseOrder;
                 if (scope.sortType !== scope.orderByAttribute) {
                     scope.sortType = scope.orderByAttribute;
-                    scope.reverseOrder = false;
-                }
-
+                    scope.reverseOrder = true;
+                }				
+				if ('sortDefault' in attrs) {
+					element.path[1].children[1].style.display = "none";
+				}
                 scope.$apply();
             });
         }
