98f60efde89d0c32e8e18ee5383674c9abdfdb63
flemmingheidepedersen <fhp@magenta-aps.dk>
2017-01-27 11:24:14 +0100

    create function added

--- app/src/sites/pd_siteService.js
+++ app/src/sites/pd_siteService.js
@@ -6,9 +6,18 @@ angular.module('openDeskApp.sites').factory('pd_siteService', function ($http, $
     return {
 
 
-        createPDSiteSite: function (siteShortName) {
-            return $http.get('/api/sites/' + siteShortName + '/memberships?authorityType=USER').then(function (response) {
-                return response.data;
+        createPDSite: function (siteName, description, sbsys, center_id, owner, manager, template) {
+            return $http.post('/alfresco/service/projectdepartment-create', {
+                PARAM_NAME: siteName,
+                PARAM_DESCRIPTION : description,
+                PARAM_SBSYS : sbsys,
+                PARAM_OWNER : owner,
+                PARAM_MANAGER : owner,
+                PARAM_CENTERID : center_id,
+                PARAM_TEMPLATE_NODEREF : template
+            }).then(function (response) {
+                console.log(response);
+                return response;
             })
         }
 
