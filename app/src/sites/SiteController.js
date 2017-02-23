c2123b478e1e02012574d4496d047889cbfb9e8a
AlexanderMomtaz <alexander@magenta.dk>
2017-02-23 15:20:23 +0100

    Removing redundant code

--- app/src/sites/SiteController.js
+++ app/src/sites/SiteController.js
@@ -361,7 +361,6 @@ angular
         }
     
         function createDocumentNotification(projekt, ref, fileName) {
-            console.log("Inside notification");
             var creatorFirstName = vm.currentUser.firstName;
             var creatorLastName = vm.currentUser.lastName;
             var creatorFullName = creatorFirstName + " " + creatorLastName;
@@ -369,7 +368,6 @@ angular
             var message = "En ny fil \"" + fileName + "\" er blevet uploadet af " + creatorFullName;
             var link = "/#!/dokument/" + ref;
 
-            console.log("Further notification");
             // Creating an empty initial promise that always resolves itself.
             var promise = $q.all([]);
 
@@ -384,13 +382,11 @@ angular
                             receiveNotifications = data[preferenceFilter];
                         }
                         if (receiveNotifications != null && receiveNotifications == "true") {
+                            console.log("Sending notification to : " + userName);
                             createNotification(userName, subject, message, link);
                         }
                     });
                 }
-            });
-
-            promise.finally(function () {
             });
         }
     
