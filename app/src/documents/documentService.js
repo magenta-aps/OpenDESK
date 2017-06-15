c12abfcf24e210aa9f6e167e83f3d24b72e75d21
Anders Jepsen <anders@magenta.dk>
2017-06-15 11:15:27 +0200

    fixed getting thumbnails when navigating history of a file

--- app/src/documents/documentService.js
+++ app/src/documents/documentService.js
@@ -52,7 +52,7 @@ function documentService($http) {
 
         //http://178.62.194.129:8080/alfresco/s/previewhelper?version_node=33ae6baa-6444-4b86-b0f2-88b8d483ae5c&parent_node=6dda80ba-9a9a-490f-9058-4fcf3da2d621
 
-        var url = '/alfresco/s/previewhelper?version_node=' + versionNode + '&parent_node=' + node + "&method=create";
+        var url = '/alfresco/s/previewhelper?version_node=' + versionNode + '&parent_node=' + node + "&method=createThumbnail";
 
         return $http.get(url).then(function (response) {
 
