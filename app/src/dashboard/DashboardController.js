f5d480bca54f6f89d993fc4db5c0dcb4e058e253
Anders Jepsen <hi@andersjepsen.com>
2017-09-22 14:22:13 +0200

    changes to the dashboard

--- app/src/dashboard/DashboardController.js
+++ app/src/dashboard/DashboardController.js
@@ -9,14 +9,14 @@ function DashboardController(pageService, headerService, APP_CONFIG) {
     headerService.setTitle('');
 
     vm.pages = [];
-    pageService.addPage(vm.pages, 'DASHBOARD.INTRA', vm.links['intra'], "home");
-    pageService.addPage(vm.pages, 'DASHBOARD.EMAIL', vm.links['email'], "mail_outline");
-    pageService.addPage(vm.pages, 'DASHBOARD.CALENDAR', vm.links['calendar'], "event");
-    pageService.addPage(vm.pages, 'DASHBOARD.PROJECTS',  vm.links['projects'], "content_paste");
-    pageService.addPage(vm.pages, 'DASHBOARD.KEY_NUMBERS', vm.links['keyNumbers'], "timeline");
-    pageService.addPage(vm.pages, 'DASHBOARD.WORK_TIME', vm.links['workTime'], "attach_money");
-    pageService.addPage(vm.pages, 'DASHBOARD.ESDH', vm.links['esdh'], "library_books");
-    pageService.addPage(vm.pages, 'DASHBOARD.CITRIX', vm.links['citrix'], "business_center");
-    pageService.addPage(vm.pages, 'DASHBOARD.MAP', vm.links['map'], "videogame_asset");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.INTRA', vm.links['intra'], "home","#b9f6ca");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.EMAIL', vm.links['email'], "mail_outline", "#ffff8d");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.CALENDAR', vm.links['calendar'], "event","#84ffff");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.PROJECTS',  vm.links['projects'], "content_paste","#80d8ff");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.KEY_NUMBERS', vm.links['keyNumbers'], "timeline","#448aff");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.WORK_TIME', vm.links['workTime'], "attach_money","#b388ff");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.ESDH', vm.links['esdh'], "library_books","#8c9eff");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.CITRIX', vm.links['citrix'], "business_center","#ff8a80");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.MAP', vm.links['map'], "videogame_asset","#ff80ab");
 
 }
