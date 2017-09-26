5ea9cd003c32be809cb61ab45159107da74d832e
Anders Jepsen <hi@andersjepsen.com>
2017-09-26 10:57:40 +0200

    styled the dashboard to be more blue

--- app/src/dashboard/DashboardController.js
+++ app/src/dashboard/DashboardController.js
@@ -9,14 +9,14 @@ function DashboardController(pageService, headerService, APP_CONFIG) {
     headerService.setTitle('');
 
     vm.pages = [];
-    pageService.addDashboard(vm.pages, 'DASHBOARD.INTRA', vm.links['intra'], "home","#b9f6ca");
-    pageService.addDashboard(vm.pages, 'DASHBOARD.EMAIL', vm.links['email'], "mail_outline", "#ffff8d");
-    pageService.addDashboard(vm.pages, 'DASHBOARD.CALENDAR', vm.links['calendar'], "event","#84ffff");
-    pageService.addDashboard(vm.pages, 'DASHBOARD.PROJECTS',  vm.links['projects'], "content_paste","#80d8ff");
-    pageService.addDashboard(vm.pages, 'DASHBOARD.KEY_NUMBERS', vm.links['keyNumbers'], "timeline","#448aff");
-    pageService.addDashboard(vm.pages, 'DASHBOARD.WORK_TIME', vm.links['workTime'], "attach_money","#b388ff");
-    pageService.addDashboard(vm.pages, 'DASHBOARD.ESDH', vm.links['esdh'], "library_books","#8c9eff");
-    pageService.addDashboard(vm.pages, 'DASHBOARD.CITRIX', vm.links['citrix'], "business_center","#ff8a80");
-    pageService.addDashboard(vm.pages, 'DASHBOARD.MAP', vm.links['map'], "videogame_asset","#ff80ab");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.INTRA', vm.links['intra'], "intra","#9c27b0");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.EMAIL', vm.links['email'], "mail", "#673ab7");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.CALENDAR', vm.links['calendar'], "calendar","#3f51b5");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.PROJECTS',  vm.links['projects'], "project","#2196f3");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.KEY_NUMBERS', vm.links['keyNumbers'], "timeline","#03a9f4");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.WORK_TIME', vm.links['workTime'], "money","#00bcd4");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.ESDH', vm.links['esdh'], "library","#009688");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.CITRIX', vm.links['citrix'], "business","#4caf50");
+    pageService.addDashboard(vm.pages, 'DASHBOARD.MAP', vm.links['map'], "map","#8bc34a");
 
 }
