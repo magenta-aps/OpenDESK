9d30df956b019b30a31f7eec25915f6d8806d590
alexandermomtaz <alexander@magenta.dk>
2017-11-29 08:58:20 +0100

    Readded method that is still being used by the move function in UI.

--- src/main/java/dk/opendesk/webscripts/FileBrowser.java
+++ src/main/java/dk/opendesk/webscripts/FileBrowser.java
@@ -48,9 +48,21 @@ public class FileBrowser extends AbstractWebScript {
         JSONArray result = new JSONArray();
 
         try {
+            NodeRef nodeRef = null;
+            String storeType = params.get("STORE_TYPE");
+            String storeId = params.get("STORE_ID");
+            String nodeId = params.get("NODE_ID");
+
+            if (storeType != null && storeId != null && nodeId != null) {
+                nodeRef = new NodeRef(storeType, storeId, nodeId);
+            }
+
             String method = params.get("method");
             if (method != null) {
                 switch (method) {
+                    case "getAll":
+                        result = getChildNodes(nodeRef);
+                        break;
                     case "getCompanyHome":
                         result = getCompanyHome();
                         break;
@@ -62,6 +74,78 @@ public class FileBrowser extends AbstractWebScript {
             webScriptResponse.setStatus(400);
         }
         Utils.writeJSONArray(webScriptWriter, result);
+    }
+
+    /**
+     * Gets child nodes.
+     * (method = getChildNodes)
+     * @param nodeRef of the parent node.
+     * @return a JSONArray containing JSONObjects for all child nodes.
+     */
+    private JSONArray getChildNodes(NodeRef nodeRef) throws JSONException {
+
+        List<ChildAssociationRef> childAssociationRefs = nodeService.getChildAssocs(nodeRef);
+
+        JSONArray result = new JSONArray();
+        JSONArray children = new JSONArray();
+        JSONObject json = new JSONObject();
+
+        String nodeName = (String) nodeService.getProperty(nodeRef, ContentModel.PROP_NAME);
+        QName nodeType = nodeService.getType(nodeRef);
+        NodeRef parentNodeRef = nodeService.getPrimaryParent(nodeRef).getParentRef();
+        QName parentNodeType = nodeService.getType(parentNodeRef);
+
+        // Users should not be able to go further up the folder hierarchy than the Sites folder
+        if (nodeType.equals(SiteModel.TYPE_SITES)) {
+            parentNodeRef = null;
+        }
+        // Document Libraries should link to Sites directly as parent and have their name
+        else if (parentNodeType.equals(SiteModel.TYPE_SITE)) {
+            nodeName = (String) nodeService.getProperty(parentNodeRef, ContentModel.PROP_NAME);
+            parentNodeRef = nodeService.getPrimaryParent(parentNodeRef).getParentRef();
+        }
+
+        if (parentNodeRef != null) {
+            json.put("primaryParent_nodeRef", parentNodeRef);
+            json.put("primaryParent_name", nodeName);
+        }
+
+        json.put("currentNodeRef_nodeRef", nodeRef.toString());
+        json.put("currentNodeRef_name", nodeName);
+
+        result.add(json);
+
+        for (ChildAssociationRef child : childAssociationRefs) {
+            JSONObject childJson = new JSONObject();
+
+            NodeRef childRef = child.getChildRef();
+
+            Map<QName, Serializable> props = nodeService.getProperties(childRef);
+            String name = (String) props.get(ContentModel.PROP_NAME);
+            QName childNodeType = nodeService.getType(childRef);
+
+            // If the child is a site then link directly to its document library
+            if (childNodeType.equals(SiteModel.TYPE_SITE)) {
+                childRef = nodeService.getChildByName(childRef, ContentModel.ASSOC_CONTAINS, SiteService.DOCUMENT_LIBRARY);
+                childNodeType = ContentModel.TYPE_FOLDER;
+            }
+            // If the child is not a site and the current node is Sites folder then it is not to be displayed
+            else if (nodeType.equals(SiteModel.TYPE_SITES)) {
+                continue;
+            }
+
+            // Only folders will be displayed
+            if (childNodeType.equals(ContentModel.TYPE_FOLDER)) {
+
+                childJson.put("nodeRef", childRef);
+                childJson.put("name", name);
+
+                children.add(childJson);
+            }
+        }
+
+        result.add(children);
+        return result;
     }
 
     /**
