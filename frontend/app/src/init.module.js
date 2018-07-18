a584c813824d4d41f5e21330a9a784b1c59da93b
AlexanderMomtaz <alexander@magenta.dk>
2018-07-18 10:09:46 +0200

    Ran JSLint

--- frontend/app/src/init.module.js
+++ frontend/app/src/init.module.js
@@ -1,25 +1,25 @@
 angular
-    .module('openDeskApp.init', ['ngMaterial'])
-    .constant('USER_ROLES', {
-        admin: 'admin',
-        user: 'user'
-        //guest: 'guest' we don't want this type of user as of yet
-    })
-    .constant('ALFRESCO_URI', {
-        apiProxy: '/alfresco/api/',
-        serviceApiProxy: '/api/',
-        serviceSlingshotProxy: '/slingshot/',
-        serviceAccessUrl: 'https://staging.opendesk.dk/alfresco/s',
-        webClientServiceProxy: '/alfresco/service'
-    })
-    .constant('PATTERNS', {
-        fileName: /^[a-zA-Z0-9_\-,!@#$%^&()=+ ]+$/,
-        phone: /^[+]?[0-9\- ]+$/
-    })
-    .constant('APP_CONFIG', {
-        sitesUrl: "grupperum",
-        landingPageState: "dashboard",
-        landingPageUrl: "",
-        ssoLoginEnabled: false
-    })
-    .constant('BROWSER_CONFIG', {});
+  .module('openDeskApp.init', ['ngMaterial'])
+  .constant('USER_ROLES', {
+    admin: 'admin',
+    user: 'user'
+    // guest: 'guest' we don't want this type of user as of yet
+  })
+  .constant('ALFRESCO_URI', {
+    apiProxy: '/alfresco/api/',
+    serviceApiProxy: '/api/',
+    serviceSlingshotProxy: '/slingshot/',
+    serviceAccessUrl: 'https://staging.opendesk.dk/alfresco/s',
+    webClientServiceProxy: '/alfresco/service'
+  })
+  .constant('PATTERNS', {
+    fileName: /^[a-zA-Z0-9_\-,!@#$%^&()=+ ]+$/,
+    phone: /^[+]?[0-9\- ]+$/
+  })
+  .constant('APP_CONFIG', {
+    sitesUrl: 'grupperum',
+    landingPageState: 'dashboard',
+    landingPageUrl: '',
+    ssoLoginEnabled: false
+  })
+  .constant('BROWSER_CONFIG', {})
