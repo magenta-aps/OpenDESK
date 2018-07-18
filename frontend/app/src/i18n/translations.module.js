6d036586e1219cb40f62913eaa61034621e561a2
AlexanderMomtaz <alexander@magenta.dk>
2018-07-18 10:02:33 +0200

    Ran JSLint

--- frontend/app/src/i18n/translations.module.js
+++ frontend/app/src/i18n/translations.module.js
@@ -1,38 +1,35 @@
 angular
-    .module('openDeskApp.translations.init', []);
+  .module('openDeskApp.translations.init', [])
 
 angular
-    .module('openDeskApp.translations', [])
-    .factory('availableLanguages', AvailableLanguages)
-    .config(config);
+  .module('openDeskApp.translations', [])
+  .factory('availableLanguages', AvailableLanguages)
+  .config(config)
 
 var availableLanguages = {
-    keys: ['en', 'da'],
-    localesKeys: {
-        'en_US': 'en',
-        'en_UK': 'en',
-        'da_DK': 'da'
-    }
-};
-
-function AvailableLanguages() {
-    return availableLanguages;
+  keys: ['en', 'da'],
+  localesKeys: {
+    'en_US': 'en',
+    'en_UK': 'en',
+    'da_DK': 'da'
+  }
 }
 
-function config($translateProvider, languageFilesProvider) {
-    languageFilesProvider.addFile('app/src/i18n/', '.json');
-    $translateProvider.useStaticFilesLoader(languageFilesProvider.getLanguageFiles());
-
-    $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
-
-    $translateProvider
-        .registerAvailableLanguageKeys(availableLanguages.keys, availableLanguages.localesKeys)
-        .determinePreferredLanguage();
-
-    //set default language if browsers langugage not found
-    if (availableLanguages.keys.indexOf($translateProvider.preferredLanguage()) === -1) {
-        $translateProvider.preferredLanguage(availableLanguages.keys[0]);
-    }
+function AvailableLanguages () {
+  return availableLanguages
 }
 
+function config ($translateProvider, languageFilesProvider) {
+  languageFilesProvider.addFile('app/src/i18n/', '.json')
+  $translateProvider.useStaticFilesLoader(languageFilesProvider.getLanguageFiles())
 
+  $translateProvider.useSanitizeValueStrategy('sanitizeParameters')
+
+  $translateProvider
+    .registerAvailableLanguageKeys(availableLanguages.keys, availableLanguages.localesKeys)
+    .determinePreferredLanguage()
+
+    // set default language if browsers langugage not found
+  if (availableLanguages.keys.indexOf($translateProvider.preferredLanguage()) === -1)
+    $translateProvider.preferredLanguage(availableLanguages.keys[0])
+}
