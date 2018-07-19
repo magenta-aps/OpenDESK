28f0dfc6406ce115b01a09be8ead4551df757e52
AlexanderMomtaz <alexander@magenta.dk>
2018-07-19 13:04:12 +0200

    Ran ESLint

--- frontend/app/src/system_settings/groups/groups.controller.js
+++ frontend/app/src/system_settings/groups/groups.controller.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
 angular
-    .module('openDeskApp.systemsettings')
-    .controller('SettingsGroupsController', SettingsGroupsController);
+  .module('openDeskApp.systemsettings')
+  .controller('SettingsGroupsController', SettingsGroupsController)
 
-function SettingsGroupsController($scope, groupService) {
-    var vm = this;
+function SettingsGroupsController ($scope, groupService) {
+  var vm = this
 
-    $scope.$watch(function () { return groupService.getOpenDeskGroups(); }, function (newVal) {
-        vm.groups = newVal;
-    }, true);
+  $scope.$watch(function () { return groupService.getOpenDeskGroups() }, function (newVal) {
+    vm.groups = newVal
+  }, true)
 
-    vm.openMemberInfo = groupService.openMemberInfo;
-    vm.editMembers = groupService.editMembers;
+  vm.openMemberInfo = groupService.openMemberInfo
+  vm.editMembers = groupService.editMembers
 }
