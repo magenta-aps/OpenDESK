695c07f4518593676e4411fed275a741de31f9e8
AlexanderMomtaz <alexander@magenta.dk>
2018-07-20 08:34:29 +0200

    ESLint

--- frontend/app/src/system_settings/notifications/NotificationsSettingsController.js
+++ frontend/app/src/system_settings/notifications/NotificationsSettingsController.js
@@ -1,34 +1,34 @@
 angular
-        .module('openDeskApp.systemsettings')
-        .controller('NotificationsSettingsController', NotificationsSettingsController);
+  .module('openDeskApp.systemsettings')
+  .controller('NotificationsSettingsController', NotificationsSettingsController)
 
-function NotificationsSettingsController($mdDialog, $translate, $state, preferenceService, UserService) {
-  var vm = this;
+function NotificationsSettingsController ($mdDialog, $translate, $state, preferenceService, UserService) {
+  var vm = this
 
-  var preferenceFilter = "dk.magenta.sites.receiveNotifications";
-  vm.receiveNotifications = "true";
-  vm.currentUser = UserService.get().userName;
+  var preferenceFilter = 'dk.magenta.sites.receiveNotifications'
+  vm.receiveNotifications = 'true'
+  vm.currentUser = UserService.get().userName
 
-  vm.loadNotificationPreferences = loadNotificationPreferences;
-  vm.setNotificationPreferences = setNotificationPreferences;
+  vm.loadNotificationPreferences = loadNotificationPreferences
+  vm.setNotificationPreferences = setNotificationPreferences
 
-  loadNotificationPreferences();
+  loadNotificationPreferences()
 
-  function loadNotificationPreferences() {
+  function loadNotificationPreferences () {
     preferenceService.getPreferences(vm.currentUser, preferenceFilter)
-    .then(function(data) {
-      if(data[preferenceFilter] != null)
-        vm.receiveNotifications = data[preferenceFilter];
-      return data[preferenceFilter];
-    });
+      .then(function (data) {
+        if (data[preferenceFilter] != null)
+          vm.receiveNotifications = data[preferenceFilter]
+        return data[preferenceFilter]
+      })
   }
 
-  function setNotificationPreferences() {
-    var preferences = { "dk.magenta.sites.receiveNotifications" : vm.receiveNotifications };
+  function setNotificationPreferences () {
+    var preferences = { 'dk.magenta.sites.receiveNotifications': vm.receiveNotifications }
 
     preferenceService.setPreferences(vm.currentUser, preferences)
-    .then(function(data) {
-      return data;
-    });
+      .then(function (data) {
+        return data
+      })
   }
 }
