0e974b7546c378e89f89c3104dcb7f03101459ea
AlexanderMomtaz <alexander@magenta.dk>
2018-07-23 11:13:57 +0200

    Split service into own function.

--- frontend/app/src/shared/services/pageService.js
+++ frontend/app/src/shared/services/pageService.js
@@ -1,35 +1,37 @@
 angular
   .module('openDeskApp')
-  .factory('pageService', function () {
-    return {
-      /**
-             * @param pages
-             * @param labelKey - key for translation
-             * @param url - of page
-             * @param icon - material icon name; default: 'content_copy'
-             */
-      addPage: function (pages, labelKey, url, icon) {
-        pages.push({
-          labelKey: labelKey,
-          url: url,
-          icon: icon || 'content_copy'
-        })
-      },
+  .factory('pageService', pageService)
 
-      /**
-             * @param pages
-             * @param labelKey - key for translation
-             * @param sref - of page
-             * @param icon - material icon name; default: 'content_copy'
-             * @param isAdminOnly - if true this page can only be viewed by admins
-             */
-      addSystemPage: function (pages, labelKey, sref, isAdminOnly, icon) {
-        pages.push({
-          labelKey: labelKey,
-          sref: sref,
-          icon: icon || 'description',
-          isAdminOnly: isAdminOnly
-        })
-      }
+function pageService () {
+  return {
+    /**
+     * @param pages
+     * @param labelKey - key for translation
+     * @param url - of page
+     * @param icon - material icon name; default: 'content_copy'
+     */
+    addPage: function (pages, labelKey, url, icon) {
+      pages.push({
+        labelKey: labelKey,
+        url: url,
+        icon: icon || 'content_copy'
+      })
+    },
+
+    /**
+     * @param pages
+     * @param labelKey - key for translation
+     * @param sref - of page
+     * @param icon - material icon name; default: 'content_copy'
+     * @param isAdminOnly - if true this page can only be viewed by admins
+     */
+    addSystemPage: function (pages, labelKey, sref, isAdminOnly, icon) {
+      pages.push({
+        labelKey: labelKey,
+        sref: sref,
+        icon: icon || 'description',
+        isAdminOnly: isAdminOnly
+      })
     }
-  })
+  }
+}
