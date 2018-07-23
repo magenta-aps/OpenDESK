a93cca80177530c17bd59da6cfc1975dc81ad1af
AlexanderMomtaz <alexander@magenta.dk>
2018-07-23 11:12:18 +0200

    Split service into own function.

--- frontend/app/src/shared/services/nodeRefUtils.service.js
+++ frontend/app/src/shared/services/nodeRefUtils.service.js
@@ -1,9 +1,11 @@
 'use strict'
 
-angular.module('openDeskApp').factory('nodeRefUtilsService', function () {
+angular.module('openDeskApp').factory('nodeRefUtilsService', nodeRefUtilsService)
+
+function nodeRefUtilsService () {
   return {
     getId: function (nodeRef) {
       return nodeRef.split('/')[3]
     }
   }
-})
+}
