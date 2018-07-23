451802a6cd88462a7d2e54dd1698f7c380e966c4
AlexanderMomtaz <alexander@magenta.dk>
2018-07-23 11:39:50 +0200

    ESLint

--- frontend/app/src/shared/services/fileUtilsService.js
+++ frontend/app/src/shared/services/fileUtilsService.js
@@ -76,8 +76,8 @@ function fileUtilsService () {
   /**
      * Copied and adapted from Alfresco share for use in opendesk. The next 3 functions that is.
      */
-  function getFileIconByMimetype (mimetype, p_iconSize) {
-    var iconSize = typeof p_iconSize === 'number' ? p_iconSize : 32
+  function getFileIconByMimetype (mimetype, pIconSize) {
+    var iconSize = typeof pIconSize === 'number' ? pIconSize : 32
     var prefix = getFiletypeByMimetype(mimetype)
 
     return prefix + '-file-' + iconSize + '.png'
@@ -92,133 +92,10 @@ function fileUtilsService () {
      * @static
      */
   function getFileExtension (filePath) {
-    var match = (new String(filePath)).match(/^.*\.([^\.]*)$/)
+    var match = (String(filePath)).match(/^.*\.([^.]*)$/)
     if (Array.isArray(match) && (typeof (match[1]) === 'string' || match instanceof String))
       return match[1]
 
     return null
-  }
-
-  /**
-     * Given a filename, returns either a filetype icon or generic icon file stem
-     *
-     * @method getFileIcon
-     * @param p_fileName {string} File to find icon for
-     * @param p_fileType {string} Optional: Filetype to offer further hinting
-     * @param p_iconSize {int} Icon size: 32
-     * @return {string} The icon name, e.g. doc-file-32.png
-     * @static
-     */
-  function getFileIcon (p_fileName, p_fileType, p_iconSize, p_fileParentType) {
-    // Mapping from extn to icon name for cm:content
-    var extns =
-        {
-          'aep': 'aep',
-          'ai': 'ai',
-          'aiff': 'aiff',
-          'asf': 'video',
-          'asnd': 'asnd',
-          'asx': 'video',
-          'au': 'audio',
-          'avi': 'video',
-          'avx': 'video',
-          'bmp': 'img',
-          'css': 'text',
-          'divx': 'video',
-          'doc': 'doc',
-          'docx': 'doc',
-          'docm': 'doc',
-          'dotx': 'doc',
-          'dotm': 'doc',
-          'eml': 'eml',
-          'eps': 'eps',
-          'fla': 'fla',
-          'flv': 'video',
-          'fxp': 'fxp',
-          'gif': 'img',
-          'htm': 'html',
-          'html': 'html',
-          'indd': 'indd',
-          'jpeg': 'img',
-          'jpg': 'img',
-          'key': 'key',
-          'mkv': 'video',
-          'mov': 'video',
-          'movie': 'video',
-          'mp3': 'mp3',
-          'mp4': 'video',
-          'mpeg': 'video',
-          'mpeg2': 'video',
-          'mpv2': 'video',
-          'msg': 'eml',
-          'numbers': 'numbers',
-          'odg': 'odg',
-          'odp': 'odp',
-          'ods': 'ods',
-          'odt': 'odt',
-          'ogg': 'video',
-          'ogv': 'video',
-          'pages': 'pages',
-          'pdf': 'pdf',
-          'png': 'img',
-          'ppj': 'ppj',
-          'ppt': 'ppt',
-          'pptx': 'ppt',
-          'pptm': 'ppt',
-          'pps': 'ppt',
-          'ppsx': 'ppt',
-          'ppsm': 'ppt',
-          'pot': 'ppt',
-          'potx': 'ppt',
-          'potm': 'ppt',
-          'ppam': 'ppt',
-          'sldx': 'ppt',
-          'sldm': 'ppt',
-          'psd': 'psd',
-          'qt': 'video',
-          'rtf': 'rtf',
-          'snd': 'audio',
-          'spx': 'audio',
-          'svg': 'img',
-          'swf': 'swf',
-          'tif': 'img',
-          'tiff': 'img',
-          'txt': 'text',
-          'wav': 'audio',
-          'webm': 'video',
-          'wmv': 'video',
-          'xls': 'xls',
-          'xlsx': 'xls',
-          'xltx': 'xls',
-          'xlsm': 'xls',
-          'xltm': 'xls',
-          'xlam': 'xls',
-          'xlsb': 'xls',
-          'xml': 'xml',
-          'xvid': 'video',
-          'zip': 'zip'
-        }
-
-    var prefix = 'generic',
-      fileType = typeof p_fileType === 'string' ? p_fileType : 'cm:content',
-      fileParentType = typeof p_fileParentType === 'string' ? p_fileParentType : null,
-      iconSize = typeof p_iconSize === 'number' ? p_iconSize : 32
-
-    // If type = cm:content, then use extn look-up
-    var type = Alfresco.util.getFileIcon.types[fileType]
-    if (type === 'file') {
-      var extn = p_fileName.substring(p_fileName.lastIndexOf('.') + 1).toLowerCase()
-      if (extn in extns)
-        prefix = extns[extn]
-    } else if (typeof type === 'undefined') {
-      if (fileParentType !== null) {
-        type = Alfresco.util.getFileIcon.types[fileParentType]
-        if (typeof type === 'undefined')
-          type = 'file'
-      } else {
-        type = 'file'
-      }
-    }
-    return prefix + '-' + type + '-' + iconSize + '.png'
   }
 }
