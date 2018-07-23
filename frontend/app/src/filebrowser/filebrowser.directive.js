91dc1c5a12ec15661e1f604dd2b03f7b01fbc806
AlexanderMomtaz <alexander@magenta.dk>
2018-07-23 15:11:28 +0200

    Changed from ngInclude to directives.

--- frontend/app/src/filebrowser/filebrowser.directive.js
+++ frontend/app/src/filebrowser/filebrowser.directive.js
@@ -5,6 +5,8 @@ import '../shared/directives/breadcrumb'
 import '../shared/directives/nodePicker'
 import '../shared/directives/sort'
 import filebrowserTemplate from './view/filebrowser.html'
+import filebrowserRowTemplate from './view/filebrowserRow.html'
+import filebrowserMenuTemplate from './actions/filebrowserMenu.html'
 
 angular
   .module('openDeskApp.filebrowser')
@@ -17,5 +19,23 @@ angular
       template: filebrowserTemplate,
       controller: 'FilebrowserController',
       controllerAs: 'vm'
+    }
+  })
+  .directive('filebrowserRow', function () {
+    return {
+      restrict: 'A',
+      scope: {
+        content: '='
+      },
+      template: filebrowserRowTemplate
+    }
+  })
+  .directive('filebrowserMenu', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        content: '='
+      },
+      template: filebrowserMenuTemplate
     }
   })
