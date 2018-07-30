945f060232932e4498a2ac7ea05043f0fce5c93d
AlexanderMomtaz <alexander@magenta.dk>
2018-07-30 12:57:03 +0200

    Fixed for Webpack

--- frontend/app/src/components/members/memberPicker/memberPicker.js
+++ frontend/app/src/components/members/memberPicker/memberPicker.js
@@ -1,34 +1,36 @@
-angular.module('members')
-.component('memberPicker', {
-  templateUrl: 'app/src/components/members/memberPicker/memberPicker.html',
-  controller: memberPicker,
-  bindings: {
-    selected: '='
-  }
-});
+'use strict'
+import memberPickerTemplate from './memberPicker.html'
+
+angular.module('openDeskApp.members')
+  .component('memberPicker', {
+    template: memberPickerTemplate,
+    controller: ['siteService', 'userService', memberPicker],
+    bindings: {
+      selected: '='
+    }
+  })
 
 function memberPicker (siteService, userService) {
-  var vm = this;
-  var owners = [];
+  var vm = this
+  var owners = []
 
-  vm.searchManagers = searchManagers;
+  vm.searchManagers = searchManagers
 
-  activate();
+  activate()
 
-  function activate() {
+  function activate () {
     siteService.getAllOwners()
-    .then(function (response) {
-        owners = response;
+      .then(function (response) {
+        owners = response
       },
       function (err) {
-        console.log(err);
+        console.log(err)
       }
-    );
+      )
   }
 
-  function searchManagers(query) {
-    if (query) {
-      return userService.getUsers(query);
-  }
+  function searchManagers (query) {
+    if (query)
+      return userService.getUsers(query)
   }
 }
