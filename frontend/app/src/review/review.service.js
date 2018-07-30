aa1d5419485d26553c6f328ca5383d36298d4485
AlexanderMomtaz <alexander@magenta.dk>
2018-07-30 13:00:09 +0200

    Fixed for Webpack

--- frontend/app/src/review/review.service.js
+++ frontend/app/src/review/review.service.js
@@ -1,45 +1,44 @@
-'use strict';
+'use strict'
 
 angular.module('od.review')
-.factory('review', review);
+  .factory('review', ['$http', review])
 
 function review ($http) {
+  var service = {
+    approve: approve,
+    create: create,
+    get: get,
+    reject: reject,
+    reply: reply,
+    update: update
+  }
 
-    var service = {
-      approve: approve,
-      create: create,
-      get: get,
-      reject: reject,
-      reply: reply,
-      update: update
-    };
+  return service
 
-    return service;
+  function get (nodeRef) {
+    console.log('get review')
+  }
 
-    function get (nodeRef) {
-      console.log('get review')
-    }
+  function create (params) {
+    console.log('create review')
+  }
 
-    function create (params) {
-      console.log('create review')
-    }
+  function approve () {
+    console.log('approve review')
+  }
 
-    function approve () {
-      console.log('approve review');
-    }
+  function reject () {
+    console.log('reject review')
+  }
 
-    function reject () {
-      console.log('reject review');
-    }
+  function reply (params) {
+    console.log('reply review')
+  }
 
-    function reply (params) {
-      console.log('reply review')
-    }
-
-    function update (params) {
-      return $http.delete("/alfresco/api/")
-          .then(function (response) {
-          return response;
-      });
-    }
+  function update (params) {
+    return $http.delete('/alfresco/api/')
+      .then(function (response) {
+        return response
+      })
+  }
 }
