20c45b82b777d24f325afb19142030e09765a12e
Alexander Momtaz Jacobsen <alexander@magenta-aps.dk>
2018-10-16 09:08:35 +0200

    Added Lool as separate edit page like OnlyOffice. Also removed a lot of unnecessary code.

--- frontend/app/src/lool/lool.module.js
+++ frontend/app/src/lool/lool.module.js
@@ -4,7 +4,6 @@ import loolTemplate from './view/lool.html'
 angular
   .module('openDeskApp.lool', [])
   .config(['$stateProvider', 'USER_ROLES', config])
-  .factory('transformRequestAsFormPost', transformRequestAsFormPost)
 
 function config ($stateProvider, USER_ROLES) {
   $stateProvider.state('lool', {
@@ -19,62 +18,11 @@ function config ($stateProvider, USER_ROLES) {
       backToDocPreview: null
     },
     views: {
-      'content@': {
+      'body@': {
         template: loolTemplate,
         controller: 'LoolController',
-        controllerAs: 'vm'
+        controllerAs: 'LC'
       }
     }
   })
-}
-/**
- * Was to be used to apply a transform for non-xhr requests.
- * Currently doesn't work due to the global interceptor.
- * @returns {transformRequest}
- */
-function transformRequestAsFormPost () {
-  /**
-     * The workhorse; converts an object to x-www-form-urlencoded serialization.
-     * @param {Object} obj
-     * @return {String}
-     */
-  function transformRequest (obj) {
-    var query = ''
-    var name
-    var value
-    var fullSubName
-    var subName
-    var subValue
-    var innerObj
-    var i
-
-    for (name in obj) {
-      value = obj[name]
-
-      if (value instanceof Array)
-        for (i = 0; i < value.length; ++i) {
-          subValue = value[i]
-          fullSubName = name + '[' + i + ']'
-          innerObj = {}
-          innerObj[fullSubName] = subValue
-          query += serializeObj(innerObj) + '&'
-        }
-
-      else if (value instanceof Object)
-        for (subName in value) {
-          subValue = value[subName]
-          fullSubName = name + '[' + subName + ']'
-          innerObj = {}
-          innerObj[fullSubName] = subValue
-          query += param(innerObj) + '&'
-        }
-
-      else if (value !== undefined && value !== null)
-        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&'
-    }
-
-    return query.length ? query.substr(0, query.length - 1) : query
-  }
-
-  return (transformRequest)
 }
--- frontend/app/src/lool/loolService.js
+++ frontend/app/src/lool/loolService.js
@@ -1,75 +1,29 @@
 angular
   .module('openDeskApp.lool')
-  .factory('loolService', ['$http', 'transformRequestAsFormPost', loolService])
+  .factory('loolService', ['$http', '$location', '$sce', loolService])
 
-function loolService ($http, transformRequestAsFormPost) {
+function loolService ($http, $location, $sce) {
   return {
-    getWopiUrl: getWopiUrl,
-    getIframeSrc: getIframeSrc,
-    getLoolServiceUrl: getLoolServiceUrl,
-    getValidMimeTypes: getValidMimeTypes
+    getLibreOfficeUrl: getLibreOfficeUrl
   }
 
-  // Just in case this is set. Call the server and see if the service url is set get around proxy issues
-  function getLoolServiceUrl () {
-    return $http.get('/lool/host/url')
-      .then(function (response) {
-        return response.data.lool_host_url
-      })
-      .catch(function () {
-        console.log('ERROR: Unable to get service url for the alfresco backend.')
-      })
+  function getLibreOfficeUrl (nodeRef, permission) {
+    return getWopiUrl(nodeRef).then(function (response) {
+      var alfrescoURL = $location.protocol() + '://' + $location.host() + '/alfresco'
+      var shortRef = nodeRef.substring(nodeRef.lastIndexOf('/') + 1)
+      var wopiFileURL = alfrescoURL + '/s/wopi/files/' + shortRef
+      var wopiSrcUrl = response.wopi_src_url + 'permission=' + permission
+      var frameSrcURL = wopiSrcUrl + '&WOPISrc=' + wopiFileURL
+      var url = frameSrcURL + '&access_token=' + response.access_token
+      url = $sce.trustAsResourceUrl(url)
+      return url
+    })
   }
 
   function getWopiUrl (nodeRef) {
-    var encNodeRef = encodeURIComponent(nodeRef)
-    return $http.get('/lool/token?nodeRef=' + encNodeRef + '&action=edit')
+    return $http.get('/lool/token?nodeRef=' + nodeRef + '&action=edit')
       .then(function (response) {
         return response.data
       })
-  }
-
-  function getValidMimeTypes () {
-    return $http.get('discovery')
-      .then(function (response) {
-        var mimeTypes = []
-        var parser = new DOMParser()
-        var xmlDoc = parser.parseFromString(response.data, 'text/xml')
-        var applications = xmlDoc.children[0].children[0].children
-        for (var id in applications)
-          if (id > -1)
-            mimeTypes.push(applications[id].getAttribute('name'))
-
-        return mimeTypes
-      })
-  }
-
-  /**
-     * Currently not in use. The idea of this was to submit a (form) request to the libreoffice online service and
-     * collect the return for use in the iFrame ro render the Loleaflet client.
-     * @param frameSrcURL
-     * @param accessToken
-     * @returns {*}
-     */
-  function getIframeSrc (frameSrcURL, accessToken) {
-    return $http({
-      method: 'POST',
-      url: frameSrcURL,
-      transformRequest: transformRequestAsFormPost,
-      data: {access_token: accessToken},
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'x-requested-with': ''
-      }
-    }).then(
-      function (response) {
-        // How to return this for an iframe
-        return response
-      },
-      function (response) {
-        // Error
-        return response
-      }
-    )
   }
 }
