1c0d2ed99d431e26731679dfbbae5149a28abcfe
Andreas Kring <andreas@magenta.dk>
2018-12-07 11:37:41 +0100

    Added license headers to all .java files and .js files

--- frontend/app/tests/e2e/cases/case.test.js
+++ frontend/app/tests/e2e/cases/case.test.js
@@ -1,3 +1,11 @@
+// 
+// Copyright (c) 2017-2018, Magenta ApS
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// 
+
 var casePage = require('./casePage.po.js');
 var loginPage = require('../login/loginPage.po.js');
 var oeUtils = require('../common/utils');
--- frontend/app/tests/e2e/members/member.test.js
+++ frontend/app/tests/e2e/members/member.test.js
@@ -1,3 +1,11 @@
+// 
+// Copyright (c) 2017-2018, Magenta ApS
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// 
+
 var constants = require('../common/constants');
 var projectHelper = require('../projects/projectHelper.js');
 var memberHelper = require('./memberHelper.js');
