e49fe285ca4739dd3c9b8eaf487da22c1a7a69aa
Flemming Heide Pedersen <fhp@magenta-aps.dk>
2019-03-12 10:21:54 +0100

    testing aos

--- backend/src/main/java/dk/opendesk/repo/override/CustomRemoteUserAuthenticatorFactory.java
+++ backend/src/main/java/dk/opendesk/repo/override/CustomRemoteUserAuthenticatorFactory.java
@@ -20,30 +20,5 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CustomRemoteUserAuthenticatorFactory extends RemoteUserAuthenticatorFactory {
 
-    @Override
-    public Authenticator create(WebScriptServletRequest req, WebScriptServletResponse res) {
-        return new CustomBasicHttpAuthenticator(req, res, this.listener);
-    }
-
-    public class CustomBasicHttpAuthenticator extends RemoteUserAuthenticator {
-        public CustomBasicHttpAuthenticator(WebScriptServletRequest req, WebScriptServletResponse res, AuthenticationListener listener) {
-            super(req, res, listener);
-        }
-
-        @Override
-        public boolean authenticate(Description.RequiredAuthentication required, boolean isGuest) {
-            boolean authenticate = super.authenticate(required, isGuest);
-            HttpServletResponse res = this.servletRes.getHttpServletResponse();
-            // Change the WWW-Authenticate header value to something else than Basic to avoid force login prompts
-            if(!authenticate && !isAdminPage())
-                res.setHeader("WWW-Authenticate", "BasicX realm=\"Alfresco\"");
-            return authenticate;
-        }
-
-        private boolean isAdminPage() {
-            HttpServletRequest req = this.servletReq.getHttpServletRequest();
-            String url = req.getRequestURL().toString();
-            return url.endsWith("alfresco/s/admin") || url.endsWith("alfresco/s/index");
-        }
-    }
+   
 }
